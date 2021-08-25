aba96f006bcfdf1a0b31aaecdf7ddbf37fa6ab2b
Manuel Ramírez López <69440193+mannyRam24-Mter@users.noreply.github.com>
2021-08-24 18:29:59 -0700

    Update exercise.py

--- assignments/01Ejercicio/src/exercise.py
+++ assignments/01Ejercicio/src/exercise.py
@@ -1,22 +1,6 @@
 
 def main():
-    peso = float(input())
-    zona = int(input())
-    if peso>0 and peso<=5000:    
-        if zona == 1:
-            print(peso*24)
-        elif zona == 2:
-            print(peso*20)
-        elif zona == 3:
-            print(peso*21)
-        elif zona == 4:
-            print(peso*12)
-        elif zona == 5:
-            print(peso*16)
-        else:
-            print("INCORRECTO")
-    else:
-        print("INCORRECTO")
+    pass
 
 
 
